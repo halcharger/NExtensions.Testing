b3d2b12c1a272eb11be3c6b61306345d9a17ff1d
halcharger <allen.firth@gmail.com>
2014-11-18 15:10:33 +0200

    initial commit

---
+++ NExtensions.Testing/AssemblyAssertionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace NExtensions.Testing
+{
+    public static class AssemblyAssertionExtensions
+    {
+        //this assertion method courtesy of Phil Haack: http://haacked.com/archive/2014/11/11/async-void-methods/
+        public static void AssertNoAsyncVoidMethods(this Assembly assembly)
+        {
+            var messages = assembly
+                .GetAsyncVoidMethods()
+                .Select(method =>
+                    String.Format("'{0}.{1}' is an async void method.",
+                        method.DeclaringType.Name,
+                        method.Name))
+                .ToList();
+            Assert.False(messages.Any(),
+                "Async void methods found!" + Environment.NewLine + String.Join(Environment.NewLine, messages));
+        }         
+    }
+}
